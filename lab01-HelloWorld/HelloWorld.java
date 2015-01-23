02051f5f16a0153a0c0afc46fb3dad71c30663c9
edp218 <edp218@lehigh.edu>
2015-01-23 15:56:05 +0000

    HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+//Edgar Palaquibay
+//Lab 1
+//Hello World Java Program
+//
+//
+//
+public class HelloWorld{
+    
+    public static void main(String[] args) {
+        
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2014!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+}
+}
