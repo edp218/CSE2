ed12b766ba7bdb5c419368fa684f61ec616ebf03
edp218 <edp218@lehigh.edu>
2015-01-27 21:25:23 +0000

    ‘WelcomeClass’

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+//Edgar Palaquibay
+//hw01
+
+
+
+
+
+
+//print the hw
+System.out.println("  -----------  ");
+System.out.println("  | WELCOME |  ");
+System.out.println("  -----------  ");
+System.out.println("  ^ ^ ^ ^ ^ ^  ");
+System.out.println("  /\/\/\/\/\/\ ");
+System.out.println("<-E--D--P--2--1--8-> ");
+System.out.println("  \/\/\/\/\/\/  ");
+System.out.println("  | WELCOME |  ");
+System.out.println("  v v v v v v  ");
+System.out.println("This Summer I visited family in Ecuador and");
+System.out.println("got robbed in the bus from Guayaquil to Quito");
